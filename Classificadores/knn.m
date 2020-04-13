ff0f45c626981f9935f4fa95c3c9a9d41f96188c
paulosantama <paulovitor_pe@hotmail.com>
2020-04-13 10:48:43 -0300

    Ajuste de parâmetros do knn de acordo com otimização do matlab

--- Classificadores/knn.m
+++ Classificadores/knn.m
@@ -1,6 +1,6 @@
 clear; close all; clc;
 
-cd('E:\Pontificia Universidade Catolica de Goias\TCC\Projeto\Utils');
+cd('E:\Pontificia Universidade Catolica de Goias\TCC\Projeto\saim\Utils');
 
 % porcentagemTreino = 0.7;
 % porcentagemTeste = 0.3;
@@ -28,11 +28,11 @@ y_test_number = grp2idx(table2array(y_test));
 
 fprintf('\n\n-> Realizando Treinamento...\n');
 tic
-Md1 = fitcknn(double(X_train),y_train_number,'NumNeighbors',5,'Standardize',1);
+Md1 = fitcknn(double(X_train),y_train_number,'NumNeighbors',6,'Distance','chebychev');
 toc
 fprintf('<- Treinamento Concluído.')
 y_predicted_md1 = predict(Md1, double(X_test));
 acuracia_md1 = sum(y_predicted_md1 == y_test_number)/length(y_test_number)*100;
 
 fprintf('\n\nExecução Finalizada.\n')
-cd('E:\Pontificia Universidade Catolica de Goias\TCC\Projeto\Classificadores');
+cd('E:\Pontificia Universidade Catolica de Goias\TCC\Projeto\saim\Classificadores');

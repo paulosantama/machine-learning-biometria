365fca9b94b78caf33399177ea23ef5f51c56cf8
paulosantama <paulovitor_pe@hotmail.com>
2020-05-21 12:21:46 -0300

    - Remoção de arquivos do projeto antigo

--- Utils/getLabelSet.m
+++
@@ -1,7 +0,0 @@
-function data = getLabelSet()
-    fprintf('\n-> Iniciando leitura do CSV com as labels...\n');
-    tic
-    data = readtable('..\Dataset\LabelSet - 16_L_N_L_BH.csv','ReadRowNames',true);
-    toc
-    fprintf('<- Finalizada a leitura do CSV com as labels.')
-end
